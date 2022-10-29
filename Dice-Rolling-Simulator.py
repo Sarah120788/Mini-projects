435d61f7325977041ddd3e8a2fd9b6a02efebdd2
Sarah120788 <109718936+Sarah120788@users.noreply.github.com>
2022-10-29 08:49:04 +0800

    Add files via upload

---
+++ Dice-Rolling-Simulator.py
@@ -0,0 +1,10 @@
+import random
+
+while True:
+    number = random.randint(1, 6)
+    dice_rolling = input('Do you want to roll again? yes or no: ')
+    if dice_rolling == 'yes':
+        print(number)
+    else:
+        print('Game over')
+        break
